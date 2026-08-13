fd03c75b38c90f10254a83e497ad7ae4b3edbad0
hjy912 <16476023+hjy912@user.noreply.gitee.com>
2026-08-13 19:23:14 +0800

    etcd优化

--- rpc/locationsvc/internal/svc/servicecontext.go
+++ rpc/locationsvc/internal/svc/servicecontext.go
@@ -1,6 +1,8 @@
 package svc
 
 import (
+	"sync"
+
 	"XiaoLong-Ridy/rpc/locationsvc/internal/config"
 	"XiaoLong-Ridy/rpc/locationsvc/internal/geo"
 	"XiaoLong-Ridy/rpc/locationsvc/internal/model"
@@ -9,6 +11,7 @@ import (
 )
 
 type ServiceContext struct {
+	mu       sync.RWMutex
 	Config   config.Config
 	Db       *gorm.DB
 	Geo      *geo.Client
@@ -27,4 +30,26 @@ func NewServiceContext(c config.Config, db *gorm.DB) *ServiceContext {
 		Geo:      geo.NewClient(c.MapService),
 		PoiModel: model.NewPoiModel(db),
 	}
+}
+
+// GetConfig 加锁读取当前配置，配合配置热更新使用
+func (s *ServiceContext) GetConfig() config.Config {
+	s.mu.RLock()
+	defer s.mu.RUnlock()
+	return s.Config
+}
+
+// GetGeo 加锁读取地图客户端，配置热更新后会重建新的客户端
+func (s *ServiceContext) GetGeo() *geo.Client {
+	s.mu.RLock()
+	defer s.mu.RUnlock()
+	return s.Geo
+}
+
+// UpdateConfig 配置热更新：替换配置并重建地图客户端
+func (s *ServiceContext) UpdateConfig(c config.Config) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	s.Config = c
+	s.Geo = geo.NewClient(c.MapService)
 }
